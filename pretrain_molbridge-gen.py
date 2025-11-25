074307936a6cd888c88567cac67f636617e75ecb
Hyuntae Park <69850926+Park-ing-lot@users.noreply.github.com>
2025-11-25 15:15:40 +0900

    Update pretrain_molbridge-gen.py

--- pretrain_molbridge-gen.py
+++ pretrain_molbridge-gen.py
@@ -22,7 +22,7 @@ parser.add_argument('--learning_rate', type=float, default=5e-4)
 
 parser.add_argument("--num_workers", type=int, default=8)
 
-parser.add_argument('--save_path', type=str, default='./outputs_pretrained_truepair_rebuttal')
+parser.add_argument('--save_path', type=str, default='./outputs_pretrained_truepair')
 parser.add_argument('--checkpoint', type=str, default='laituan245/molt5-base')
 parser.add_argument('--data_dir', type=str, default=YOUR_DATA_DIR_PATH)
 parser.add_argument('--dataset', type=str, default='ChEBI-20_data', choices=["pubchemstm", "ChEBI-20_data"])
